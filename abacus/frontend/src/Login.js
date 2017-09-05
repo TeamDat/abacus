7ff81793e7dfc6478473a489dc5e1fc74332fe9f
Logan Gillespie <logancgillespie@gmail.com>
2017-09-05 14:13:10 -0400

    database rule change as well as some cleanup

--- abacus/frontend/src/Login.js
+++ abacus/frontend/src/Login.js
@@ -37,7 +37,7 @@ export default class Login extends Component {
           {
             this.state.loginMessage &&
             <div className="alert alert-danger" role="alert">
-              <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+              <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"/>
               <span className="sr-only">Error:</span>
               &nbsp;{this.state.loginMessage} <button onClick={resetPassword} className="alert-link">Forgot Password?</button>
             </div>
