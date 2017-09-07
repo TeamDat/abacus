d80984dc8ecaae3fc6b29505052e5074c1940f25
Logan Gillespie <logancgillespie@gmail.com>
2017-09-07 14:00:29 -0400

    add copyright statement required by 3312 class requirements

--- abacus/frontend/src/Header.js
+++ abacus/frontend/src/Header.js
@@ -1,14 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) Team.Dat / Team 7147 GATech.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Ian Ewell, Logan Gillespie, Ginna Groover, Brighton Trugman, Caroline Zhang
+ *******************************************************************************/
+
 import React from 'react';
 import Navbar from 'react-bootstrap/lib/Navbar'
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
 import {logout} from './auth';
 
-
 export default class Header extends React.Component {
     render() {
         return (
-            <Navbar inverse >
+            <Navbar inverse>
                     <Navbar.Brand>
                         <a href='/'>Abacus</a>
                     </Navbar.Brand>
