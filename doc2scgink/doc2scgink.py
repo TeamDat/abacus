4283164e9d16fb2c635219b956bfdb336cf6e688
iewell <ianewellgsmst@gmail.com>
2017-11-17 03:42:14 -0500

    Add preliminary box vectorizer, which is not quite ready for production yet as path extraction isn't implemented

--- doc2scgink/doc2scgink.py
+++ doc2scgink/doc2scgink.py
@@ -1,8 +1,9 @@
+from collections import deque
 import numpy as np
 import imageio
 import sys
 
-#import visvis as vv
+import visvis as vv
 
 def load_image(file_name):
     handle = open(file_name, "r")
@@ -61,7 +62,7 @@ def create_blobs(bitmap):
                     bounds[label-1][3] += (c0-c)
                 if c > c1:
                     bounds[label-1][3] += (c-c1)
-
+                
                 # Add unlabeled neighbors to queue
                 if r + 1 < height and bitmap[r + 1, c] == False and labels[r + 1, c] == 0:
                     labels[r + 1, c] = label
@@ -85,11 +86,18 @@ def create_blobs(bitmap):
         # Reject the blob if its area is below a threshold
         if bounds[i][2] * bounds[i][3] <= 30:
             continue
+        # Add a 10px black boarder around the blob as a vecotrizer algorithm depends on it
+        bounds[i][0] -= 10
+        bounds[i][1] -= 10
+        bounds[i][2] += 20
+        bounds[i][3] += 20
+
+        # Extract the bitmap for this blob from the main image
         bitmap = labels == i + 1
         bitmap = bitmap[bounds[i][0] : bounds[i][0]+bounds[i][2], bounds[i][1] : bounds[i][1]+bounds[i][3]]
         blobs.append(((bounds[i][0], bounds[i][1], bounds[i][2], bounds[i][3]), bitmap))
 
-    return blobs
+    return blobs 
 
 # Attempt to recover a list of strokes for a given blob
 def vectorize_blob_horizontal_scan(blob, absolute_coords=False):
@@ -119,10 +127,7 @@ def vectorize_blob_horizontal_scan(blob, absolute_coords=False):
     return (stroke_x, stroke_y)
 
 def vectorize_blob_vertical_scan(blob, absolute_coords=False):
-    # This implements a naive horizontal scanline based vectorizer which is pretty
-    # much only good for the "1" character
-
-    # Extract a sequence of ranges and the median for each horizontal scanline
+    # Use vertical scanlines instead of horizontal ones
     bitmap = blob[1]
     stroke_x = []
     stroke_y = []
@@ -143,6 +148,209 @@ def vectorize_blob_vertical_scan(blob, absolute_coords=False):
             stroke_y.append((mini + maxi) / 2)
             stroke_x.append(col)
     return (stroke_x, stroke_y)
+
+# Counts the number of corners in a region that are "on" in a bitmap
+def corner_count(bitmap, box):
+    row = box[0]
+    col = box[1]
+    height = box[2]
+    width = box[3]
+    count = 0
+    if bitmap[row, col] == True:
+        count += 1
+    if bitmap[row+height-1, col] == True:
+        count += 1
+    if bitmap[row, col+width-1] == True:
+        count += 1
+    if bitmap[row+height-1, col+width-1] == True:
+        count += 1
+    return count
+
+# Return a list of plottable points for a box
+def box_coords(box):
+    row = box[0]
+    col = box[1]
+    height = box[2]
+    width = box[3]
+    y = np.array([row, row+height-1, row+height-1, row, row])
+    x = np.array([col, col, col+width-1, col+width-1, col])
+    return (x, y)
+
+# Returns the "mass" or the number of lit pixels in a box
+def box_mass(blob, box):
+    row = box[0]
+    col = box[1]
+    height = box[2]
+    width = box[3]
+    a = blob[row : row + height, col : col + width]
+    return np.apply_over_axes(np.sum, a, [0,1])[0][0]
+
+def box_coverage(blob, box):
+    return float(box_mass(blob, box)) / float(box[2] * box[3])
+
+# Center of mass used to derive a point from a box
+def box_center_of_mass(blob, box):
+    row = box[0]
+    col = box[1]
+    height = box[2]
+    width = box[3]
+    c_x = 0
+    c_y = 0
+    mass = 0
+    for x in range(col, col+width):
+        for y in range(row, row+height):
+            if blob[y, x]:
+                c_x += x
+                c_y += y
+                mass += 1
+    return (c_x/mass, c_y/mass)
+
+# Uses hill climbing to shift a box along an axis to locally maximize fill
+def box_optimize_fill(blob, box, axis):
+    row = box[0]
+    col = box[1]
+    height = box[2]
+    width = box[3]
+
+    shift_limit = height if axis[0] else width
+    shift_limit /= 2
+
+    fill = box_coverage(blob, box)
+    fill_neg = 0.0
+    if row - axis[0] >= 0 and col - axis[1] >= 0:
+        fill_neg = box_coverage(blob, [row - axis[0], col - axis[1], height, width])
+    fill_pos = 0.0
+    if row + height + axis[0] <= blob.shape[0] and col + width + axis[1] <= blob.shape[1]:
+        fill_pos = box_coverage(blob, [row + axis[0], col + axis[1], height, width])
+    while (fill < fill_neg or fill < fill_pos) and shift_limit > 0:
+        shift_limit -= 1
+        if fill_neg > fill_pos:
+            row -= axis[0]
+            col -= axis[1]
+            fill_pos = fill
+            fill = fill_neg
+            fill_neg = 0.0
+            if row - axis[0] >= 0 and col - axis[1] >= 0:
+                fill_neg = box_coverage(blob, [row - axis[0], col - axis[1], height, width])
+        else:
+            row += axis[0]
+            col += axis[1]
+            fill_neg = fill
+            fill = fill_pos
+            fill_pos = 0.0
+            if row + height + axis[0] <= blob.shape[0] and col + width + axis[1] <= blob.shape[1]:
+                fill_pos = box_coverage(blob, [row + axis[0], col + axis[1], height, width])
+    return [row, col, height, width]
+
+
+# Returns the neighboring boxes for a box and their directions
+def box_neighbors(blob, box):
+    # To determine if a neighbor exists, we use two factors:
+    # fill percentage and connectedness
+    minimum_fill_a = 0.15
+    minimum_fill_b = 0.4
+    neighbors = []
+    
+    row = box[0]
+    col = box[1]
+    height = box[2]
+    width = box[3]
+
+    # Check all the neighbors
+    north = [row-height, col, height, width]
+    if north[0] >= 0 and box_coverage(blob, north) > minimum_fill_a:
+        north = box_optimize_fill(blob, north, (0, 1))
+        if box_coverage(blob, north) > minimum_fill_b:
+            neighbors.append((north, (-1, 0)))
+    south = [row+height, col, height, width]
+    if south[0] + south[2] <= blob.shape[0] and box_coverage(blob, south) > minimum_fill_a:
+        south = box_optimize_fill(blob, south, (0, 1))
+        if box_coverage(blob, south) > minimum_fill_b:
+            neighbors.append((south, (1, 0)))
+    west = [row, col-width, height, width]
+    if west[1] >= 0 and box_coverage(blob, west) > minimum_fill_a:
+        west = box_optimize_fill(blob, west, (1, 0))
+        if box_coverage(blob, west) > minimum_fill_b:
+            neighbors.append((west, (0, -1)))
+    east = [row, col+width, height, width]
+    if east[1] + east[3] <= blob.shape[1] and box_coverage(blob, east) > minimum_fill_a:
+        east = box_optimize_fill(blob, east, (1, 0))
+        if box_coverage(blob, east) > minimum_fill_b:
+            neighbors.append((east, (0, 1)))
+
+    # Sort by coverage
+    def coverage(a):
+        return box_coverage(blob, a[0])
+
+    return sorted(neighbors, key=coverage, reverse=True)
+
+def vectorize_blob_boxgrid(blob, absolute_coords=False):
+    # This vectorizer attemps to recover strokes by approximating the character
+    # with a graph of connecting axis-aligned squares that reasemble the character.
+    # The strokes are then reconstructed using graph algorithms to find paths that
+    # traverse the graph
+    bitmap = blob[1]
+    bitmap_copy = np.array(blob[1])
+
+    # Start the first square from the first pixel in the image. Probably not the best
+    # heuristic but it's quick
+    first_pixel = np.nonzero(bitmap[10])[0][0]
+    first_box = [9, first_pixel-1, 2, 2]
+    
+    # Use a heuristic to get a decent first box: first expand the box to the bottom-right
+    # until at least two of the corners of the box have part of the blob, and then expand
+    # until one or fewer corners are "lit"
+    corners = 1
+    while corners == 1:
+        first_box[2] += 1
+        first_box[3] += 1
+        corners = corner_count(bitmap, first_box)
+    if corners > 1:
+        while corners > 1:
+            first_box[2] += 1
+            first_box[3] += 1
+            corners = corner_count(bitmap, first_box)
+
+    # Use a coverage based heuristic to extend in the bottom left direction to get a better
+    # first box
+    cvg = box_coverage(bitmap, first_box)
+    print cvg
+    target_cvg = cvg*0.95
+    while cvg > target_cvg and corners > 0:
+        first_box[1] -= 1
+        first_box[2] += 1
+        first_box[3] += 1
+        corners = corner_count(bitmap, first_box)
+        cvg = box_coverage(bitmap, first_box)
+        print "   " + str(cvg)
+
+    # Clear the box from the bitmap and find the neighboring boxes
+    bitmap[first_box[0] : first_box[0] + first_box[2], first_box[1] : first_box[1] + first_box[3]] = False
+    neighbors = box_neighbors(bitmap, first_box)
+
+    # Use BFS to get all the other squares
+    squares = [first_box]
+    queue = deque(neighbors)
+    while len(queue) > 0:
+        box = queue.pop()[0]
+        if box_coverage(bitmap, box) < 0.4:
+            continue
+        bitmap[box[0] : box[0] + box[2], box[1] : box[1] + box[3]] = False
+        for n in box_neighbors(bitmap, box):
+            queue.append(n)
+        squares.append(box)
+
+    xs = []
+    ys = []
+    for n in squares:
+        x, y = box_coords(n)
+        vv.plot(x, y, lc='r', ms='.', mc='g', mw=2, lw=3)
+        x, y = box_center_of_mass(bitmap_copy, n)
+        print x
+        print y
+        xs.append(x)
+        ys.append(y)
+    return (xs, ys)
 
 def generate_scgink(blobs, output_file):
     strokes = []
@@ -166,17 +374,16 @@ greyscale = convert_to_greyscale(image)
 threshold = threshold_image(greyscale, 128)
 blobs = create_blobs(threshold)
 generate_scgink(blobs, sys.argv[2])
-"""
 fig = vv.figure()
 fig.position.w = 700
 for i in range(len(blobs)):
-    if blobs[i][0][2] > blobs[i][0][3]:
-        x, y = vectorize_blob_horizontal_scan(blobs[i])
-    else:
-        x, y = vectorize_blob_vertical_scan(blobs[i])
+    #if blobs[i][0][2] > blobs[i][0][3]:
+    #    x, y = vectorize_blob_horizontal_scan(blobs[i])
+    #else:
+    #    x, y = vectorize_blob_vertical_scan(blobs[i])
     vv.subplot(4, 4, i+1)
     vv.imshow(blobs[i][1])
-    vv.plot(x, y, lc='r', ms='.', mc='g', mw=4, lw=2)
+    x, y = vectorize_blob_boxgrid(blobs[i])
+    vv.plot(x, y, lc='r', ms='.', mc='g', mw=2, lw=1)
 app = vv.use()
 app.Run()
-"""
